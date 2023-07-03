8813088053c7cda1dfd1de9852056b93fe149d33
vicentecarloscisnerosolivo <vicente.carlos@live.com.mx>
2023-07-02 20:39:06 -0500

    Change Typo
    Change typo in Schools Fragment

--- app/src/main/java/com/vcco/a20230704_vcco_nycschools/ui/schools/SchoolFragment.kt
+++ app/src/main/java/com/vcco/a20230704_vcco_nycschools/ui/schools/SchoolFragment.kt
@@ -8,24 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import com.vcco.a20230704_vcco_nycschools.R
 
-class Schoolsragment : Fragment() {
+class SchoolFragment : Fragment() {
 
     companion object {
-        fun newInstance() = Schoolsragment()
+        fun newInstance() = SchoolFragment()
     }
 
-    private lateinit var viewModel: SchoolsragmentViewModel
+    private lateinit var viewModel: SchoolViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_schoolsragment, container, false)
+        return inflater.inflate(R.layout.fragment_schools, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SchoolsragmentViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(SchoolViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/com/vcco/a20230704_vcco_nycschools/ui/schools/SchoolViewModel.kt
+++ app/src/main/java/com/vcco/a20230704_vcco_nycschools/ui/schools/SchoolViewModel.kt
@@ -2,6 +2,6 @@ package com.vcco.a20230704_vcco_nycschools.ui.schools
 
 import androidx.lifecycle.ViewModel
 
-class SchoolsragmentViewModel : ViewModel() {
+class SchoolViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
